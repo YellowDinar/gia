f8840fcec1d1352f55d792744b247a78e109655d
Artur <otejio@gmail.com>
2015-06-22 16:38:10 +0300

    bugs

--- gia/urls.py
+++ gia/urls.py
@@ -6,12 +6,12 @@ from gia import settings
 from account.views import MyRegistrationView
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('online_test.urls')),
-    # url(r'^admin_tools/', include('admin_tools.urls')),
-    url(r'^account/', include('account.urls')),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('online_test.urls')),
+    url(r'^account/', include('account.urls')),
+
 )
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
