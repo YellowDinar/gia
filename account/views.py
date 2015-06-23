0bc3fc4967297a84ebbd252538b0bac1c5942bab
Artur <otejio@gmail.com>
2015-06-23 09:10:40 +0300

    bugs

--- account/views.py
+++ account/views.py
@@ -4,4 +4,4 @@ from registration.backends.simple.views import RegistrationView
 # Create a new class that redirects the user to the index page, if successful at logging
 class MyRegistrationView(RegistrationView):
     def get_success_url(self, request, user):
-        return '/'
+        return 'index'
