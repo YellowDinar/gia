798257a7902e01122bf0d47904a785cff8b5f498
yellowdinar <kalimullindinrust@gmail.com>
2015-06-25 15:24:19 +0300

    дебагинг урлов

--- account/views.py
+++ account/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth import views as auth_views
 # Create a new class that redirects the user to the index page, if successful at logging
 class MyRegistrationView(RegistrationView):
     def get_success_url(self, request, user):
-        return 'index'
+        return 'result'
